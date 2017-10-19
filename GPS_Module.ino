7ad6868f130f14a1b5e2c0b539b5cd0d53dea26d
cya-ga-umai <cya-ga@umai.com>
2017-10-19 17:04:36 +0900

    [add]NMEA読み取り関数の作成

--- GPS_Module.ino
+++ GPS_Module.ino
@@ -10,13 +10,32 @@
 
 SoftwareSerial gps(PIN_GPS_RX, PIN_GPS_TX);
 
+// NMEAフォーマット読み取り関数
+void rcv_nmea(char *buf){
+    char c;
+    int count = 0;
+
+    do {
+        if (gps.available()) {
+            buf[count] = gps.read();
+            count++;
+        }
+        if (count > BUFFER_SIZE - 2) break; // バッファ溢れを監視する
+    } while (buf[count - 1] != 0x0a);
+    buf[count] = '\0';
+}
+
 void setup(){
     Serial.begin(SERIAL_BAUDRATE);
     gps.begin(GPS_BAUDRATE);
 }
 
 void loop(){
-    if (gps.available()) {
-        Serial.write(gps.read());
+    char buf[BUFFER_SIZE];
+
+    rcv_nmea(buf);
+
+    if (!strncmp("$GPGGA", buf, 6)) {
+        Serial.print(buf);
     }
 }

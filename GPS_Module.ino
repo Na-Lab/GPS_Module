848a9b1bdc3a17e3f83f174dc0ab292b735b4d0f
cya-ga-umai <cya-ga@umai.com>
2017-10-20 22:56:32 +0900

    [clean]フォーマットを統一

--- GPS_Module.ino
+++ GPS_Module.ino
@@ -12,63 +12,73 @@
 
 SoftwareSerial gps(PIN_GPS_RX, PIN_GPS_TX);
 
-typedef struct position {
-    double latitude;    // 緯度
-    double longitude;   // 軽度
+typedef struct position
+{
+    double latitude;  // 緯度
+    double longitude; // 軽度
 } position_t;
 
 // NMEAフォーマット読み取り関数
-void rcv_nmea(char *buf) {
+void rcv_nmea(char *buf)
+{
     char c;
     int count = 0;
 
-    do {
-        if (gps.available()) {
+    do
+    {
+        if (gps.available())
+        {
             buf[count] = gps.read();
             count++;
         }
-        if (count > BUFFER_SIZE - 2) break; // バッファ溢れを監視する
+        if (count > BUFFER_SIZE - 2)
+            break; // バッファ溢れを監視する
     } while (buf[count - 1] != 0x0a);
     buf[count] = '\0';
 }
 
 // DEG表記変換関数
-double dmm_2_deg(char *data){
+double dmm_2_deg(char *data)
+{
     double temp, deg, min;
 
     temp = atof(data);
-    deg = (int)(temp/100);
+    deg = (int)(temp / 100);
     min = temp - deg * 100;
     return (double)(deg + min / 60.0);
 }
 
 // 位置情報取得関数
-void get_position(position_t *pos) {
+void get_position(position_t *pos)
+{
     char buf[BUFFER_SIZE];
     char *lat_p, *long_p;
 
     // NMEAの取得
-    do {
+    do
+    {
         rcv_nmea(buf);
-    } while(strncmp("$GPGGA", buf, 6));
+    } while (strncmp("$GPGGA", buf, 6));
 
     // 位置情報の抽出
-    strtok(buf, DELIMITER );            // $GPGGA
-    lat_p = strtok(NULL, DELIMITER );   // 緯度
-    strtok(NULL, DELIMITER);            // 南北
-    long_p = strtok(NULL, DELIMITER);   // 経度
+    strtok(buf, DELIMITER);           // $GPGGA
+    lat_p = strtok(NULL, DELIMITER);  // 緯度
+    strtok(NULL, DELIMITER);          // 南北
+    long_p = strtok(NULL, DELIMITER); // 経度
 
     // データの加工
-    pos->latitude   = dmm_2_deg(lat_p);
-    pos->longitude  = dmm_2_deg(long_p);
+    pos->latitude = dmm_2_deg(lat_p);
+    pos->longitude = dmm_2_deg(long_p);
 }
 
-void setup(){
+void setup()
+{
     Serial.begin(SERIAL_BAUDRATE);
     gps.begin(GPS_BAUDRATE);
 }
 
-void loop(){
+void loop()
+{
     position_t pos = {0};
 
     get_position(&pos);

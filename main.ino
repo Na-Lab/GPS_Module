3dcf1f7a4f0478cf0125a14f11ad384388a9164d
cya-ga-umai <myon.ibuki@gmail.com>
2017-11-09 16:41:03 +0900

    [change]NMEAをString型で受信するように変更

--- main.ino
+++ main.ino
@@ -1,19 +1,17 @@
 #include "GPS_Module.h"
+#define SERIAL_BAUDRATE 9600  // シリアルモニタビューレート
 
-void setup()
-{
-    Serial.begin(SERIAL_BAUDRATE);
-    gps_init();
+void setup() {
+  Serial.begin(SERIAL_BAUDRATE);
+  gps_init();
 }
 
-void loop()
-{
-    position_t pos = {0};
+void loop() {
+  position_t pos = {0};
+  get_position(&pos);
 
-    get_position(&pos);
-
-    Serial.print("Latitude = ");
-    Serial.print(pos.latitude);
-    Serial.print(", Longitude = ");
-    Serial.println(pos.longitude);
+  Serial.print("Latitude = ");
+  Serial.print(pos.latitude);
+  Serial.print(", Longitude = ");
+  Serial.println(pos.longitude);
 }
